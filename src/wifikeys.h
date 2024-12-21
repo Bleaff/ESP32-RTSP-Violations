09247e289b65ff8e71966b9b2fb9058dc5d4a090
Anonimus Sergey <sergey3010.01@mail.ru>
2024-12-21 22:25:56 +0300

    Updated cstreamer

--- src/wifikeys.h
+++ src/wifikeys.h
@@ -1,4 +1,4 @@
 #include <Arduino.h>
 // Change YOUR_AP_NAME and YOUR_AP_PASSWORD to your WiFi credentials
-const char *ssid = "YOUR_AP_NAME";		   // Put your SSID here
-const char *password = "YOUR_AP_PASSWORD"; // Put your PASSWORD here
+const char *ssid = "Eco-24";		   // Put your SSID here
+const char *password = "9252696364"; // Put your PASSWORD here
